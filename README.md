24e3ff8b9358b730996602ef26fa7423ce0a3559
flowcheck <71723824+flowcheck@users.noreply.github.com>
2021-04-17 01:46:26 -0400

    structure

--- README.md
+++ README.md
@@ -1,24 +1,11 @@
-# Music Mixer Assignment Checkpoint 1 (Jackson & Jack)
+# Music Mixer Assignment Checkpoint 2 (Jackson & Jack)
 	
 This project was made for our Interactive Media Design class.
 
-## Assignment Details (1st checkpoint)
+## Assignment Details (2nd checkpoint)
 
-For this portion of the assignment we have to layout a rough draft of the final project.
-
-We have to include:
-
-- Github repos for each project with team access (both members contributing).
-
-- Readme file that describes the project, and any technical requirements for local
-setup / development. <!-- (DONE) -->
-
-- Initial commits from the developer and the designer: site shells / project structure (files
-and folders up on the repos - can be empty with .gitkeep files).
-
-- Dev notes for the projects.
-
-- Pinterest or other collections of examples / inspiration.
+In this checkpoint we weren't able to figure out how to get the D&D feature to work correctly, we were able to get the first part to work
+but nothing would come after the first part where it confirms that you have grabbed an image
 
 ## Features
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,71 @@
 (()=> {
-	console.log('js is linked up');
+	console.log('JS is linked up');
 })();
+
+	const dragImages = document.querySelectorAll(".draggable"),
+		leftDrop = document.querySelector(".dropL"),
+		rightDrop = document.querySelector(".dropR"),
+		dropZone = document.querySelectorAll ('.dropZone'),
+		dragArea = document.querySelector(".dragArea");
+
+	
+	//----functions----//
+
+function dragStart(event) {
+	console.log('hey quit dragging me around');
+	event.dataTransfer.setData("savedID", this.id);
+}
+
+function draggedOver(event) {
+	event.preventDefault();
+	console.log('please put me down');
+}
+
+function dropped(event) {
+	console.log('thanks for putting me down, enjoy the tunes');
+	
+
+	let targetID = event.dataTransfer.getData("savedID"),
+	audioPlay = document.querySelector(`audio[data-value="${targetID}"]`);
+
+
+	if (this.childNodes.length > 0) { return; }
+	console.log("Now playing:", targetID, "!");
+	this.appendChild(document.querySelector(`#${targetID}`));
+
+
+	if (!audioPlay) { return; }
+	audioPlay.currentTime = 0;
+	audioPlay.play();
+	audioPlay.volume = 0.4;
+	
+	}
+
+
+
+	//----event handling----//
+
+	document.addEventListener('dragstart' , (e) => {
+		e.target.style.border = '2px dashed blue';
+	}, false);
+
+	document.addEventListener('dragend', (e) => {
+		e.target.style.border = 'none';
+	}, false);
+
+	dragImages.forEach(piece => piece.addEventListener('dragstart', dragStart));
+	
+	dropZone.forEach(zone => {
+		zone.addEventListener("dragover", draggedOver);
+		zone.addEventListener("drop", dropped);
+	})
+
+
+
+
+
+
+
+
+
+
